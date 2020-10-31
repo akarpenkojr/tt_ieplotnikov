950522ef12c587e56dcbfa6c04aa4b6bf0e77c6e
akarpenko_jr <andy.karpenko737@gmail.com>
2020-10-31 21:32:20 +0300

    Changed animation time of main menu hamburger

--- src/components/Hamburger.vue
+++ src/components/Hamburger.vue
@@ -69,7 +69,7 @@ export default {
   border-radius: 2px;
   position: absolute;
   transition-property: transform;
-  transition-duration: 0.15s;
+  transition-duration: 0.35s;
   transition-timing-function: ease;
 }
 .hamburger-inner::before,
@@ -84,15 +84,15 @@ export default {
   bottom: -6px;
 }
 .hamburger--squeeze .hamburger-inner {
-  transition-duration: 0.075s;
+  transition-duration: 0.175s;
   transition-timing-function: cubic-bezier(0.55, 0.055, 0.675, 0.19);
 }
 .hamburger--squeeze .hamburger-inner::before {
-  transition: top 0.075s 0.12s ease, opacity 0.075s ease;
+  transition: top 0.175s 0.12s ease, opacity 0.175s ease;
 }
 .hamburger--squeeze .hamburger-inner::after {
-  transition: bottom 0.075s 0.12s ease,
-    transform 0.075s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  transition: bottom 0.175s 0.12s ease,
+    transform 0.175s cubic-bezier(0.55, 0.055, 0.675, 0.19);
 }
 
 .hamburger--squeeze.is-active .hamburger-inner {
@@ -105,13 +105,13 @@ export default {
   background-color: #ff238d;
   top: 0;
   opacity: 0;
-  transition: top 0.075s ease, opacity 0.075s 0.12s ease;
+  transition: top 0.175s ease, opacity 0.175s 0.12s ease;
 }
 .hamburger--squeeze.is-active .hamburger-inner::after {
   background-color: #ff238d;
   bottom: 0;
   transform: rotate(-90deg);
-  transition: bottom 0.075s ease,
-    transform 0.075s 0.12s cubic-bezier(0.215, 0.61, 0.355, 1);
+  transition: bottom 0.175s ease,
+    transform 0.175s 0.12s cubic-bezier(0.215, 0.61, 0.355, 1);
 }
 </style>
